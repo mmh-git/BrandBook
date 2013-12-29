5c3040331b75ab3dc5b977376bd6534140f3ffc0
swapnarogi <mhlalin@live.com>
2013-12-30 01:32:50 +0600

    main content slide style change. width height fixed

--- BrandBook/App_Start/BundleConfig.cs
+++ BrandBook/App_Start/BundleConfig.cs
@@ -12,7 +12,8 @@ namespace BrandBook
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/jquery-ui-{version}.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate.js",
--- BrandBook/obj/Debug/Package/PackageTmp/Scripts/user/index.js
+++ BrandBook/obj/Debug/Package/PackageTmp/Scripts/user/index.js
@@ -5,38 +5,42 @@ $(document).ready(function () {
     $.ajax({
         url: baseUrl + "Account/IsLogedIn", type: 'GET', async: false, success: function (result) {
             if (result.loggedIn) {
-                var statUrl = baseUrl + "User/Index";
-                //$("#divMain").css("height", mainHeight);
-                //$("#content").css("height", mainHeight - 4);
-                    $.ajax({
-                        url: statUrl, type: 'GET', async: false, success: function (result) {
-                            $("#StatusMain").html(result);
-                        }
-                    });
-                    $("#divMain").css('display','block');
+                //window.ShowLoading(true);
+                //var statUrl = baseUrl + "User/Index";
+                ////$("#divMain").css("height", mainHeight);
+                ////$("#content").css("height", mainHeight - 4);
+                //$.ajax({
+                //    url: statUrl, type: 'GET', async: false, success: function (result) {
+                //        $("#StatusMain").html(result);
+                //        window.ShowLoading(false);
+                //    }
+                //});
+                //$("#divMain").css('display', 'block');
 
-                $("<a id='aLogout' href='#'>Log Out</>").appendTo("#divLogOut");
-                $("#aLogout").on("click", function () {
-                    
-                    $.ajax({
-                        url: baseUrl + "Account/LogOff",
-                        type: 'POST',
-                        success: function (data) {
-                            if (data == "success") {
-                                $("#divMain").slideUp("slow", function () {
-                                    $("#loginContent").fadeIn({ duration: 500 });
-                                    $("#divLogOut").fadeOut({ duration: 500 }).empty();
-                                    clearInterval(setIntervalId);
-                                });
-                                
-                            }
-                        }
-                    });
-                    
-                    });
+                loadUser();
 
-                    $("#divLogOut").css('display','block');
-                }
+                //$("<a id='aLogout' href='#'>Log Out</>").appendTo("#divLogOut");
+                //$("#aLogout").on("click", function () {
+
+                //    $.ajax({
+                //        url: baseUrl + "Account/LogOff",
+                //        type: 'POST',
+                //        success: function (data) {
+                //            if (data == "success") {
+                //                $("#divMain").slideUp("slow", function () {
+                //                    $("#loginContent").fadeIn({ duration: 500 });
+                //                    $("#divLogOut").fadeOut({ duration: 500 }).empty();
+                //                    clearInterval(setIntervalId);
+                //                });
+
+                //            }
+                //        }
+                //    });
+
+                //});
+
+                //$("#divLogOut").css('display', 'block');
+            }
         }
     });
 });
@@ -49,10 +53,12 @@ function loadUser() {
     var statUrl = baseUrl + "User/Index";
     //$("#divMain").css("height", mainHeight);
     //$("#content").css("height", mainHeight - 4);
+    window.ShowLoading(true);
     $("#loginContent").fadeOut(function () {
         $.ajax({
-            url: statUrl, type: 'GET', async: false, success: function (result) {
+            url: statUrl, type: 'GET', async: true, success: function (result) {
                 $("#StatusMain").html(result);
+                window.ShowLoading(false);
             }
         });
         $("#divMain").slideDown({ duration: 500 });
@@ -81,4 +87,24 @@ function loadUser() {
     //$("#loginContent").animate({ height: 0, duration: 600 });
 
     //loginContent
+}
+
+window.ShowLoading = function (block)
+{
+    if (block) {
+        $.blockUI({
+
+            message: "<img src='" + baseUrl + "Content/themes/base/images/wait.gif' />",
+            css: {
+                top: ($(window).height() * 50 / 100) + 'px',
+                left: ($(window).width() - 32) / 2 + 'px',
+                width: '40px',
+                height: '40px'
+            }
+        });
+    }
+    else
+    {
+        $.unblockUI();
+    }
 }
